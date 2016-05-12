526c0261f4cddbe13b597192285d3144b7aaac85
Courtney Nguyen <cnguyen@1800contacts.com>
2016-05-11 22:12:19 -0600

    Exporting dragdropmanager

--- src/index.es6.js
+++ src/index.es6.js
@@ -1,2 +1,3 @@
 export {default as Draggable} from './components/Draggable.es6';
 export {default as DropTarget} from './components/DropTarget.es6';
+export {default as DragDropManager} from './DragAndDropManager.es6';
