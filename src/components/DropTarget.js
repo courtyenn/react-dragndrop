7202fdac2087509d448b4fecae5c823fff78db6d
Courtney Nguyen <cnguyen@1800contacts.com>
2016-06-04 18:24:15 -0600

    all the commits

--- src/components/DropTarget.js
+++ src/components/DropTarget.js
@@ -77,7 +77,7 @@ export default class DropTarget extends Component{
       var allTheChildren = [];
       this.props.children.forEach((child, index) => {
         var keyProps = Object.assign({}, child.props, {key: index + "-droptarget-module"});
-        if(child.props.children === '' || typeof child.props.children === 'undefined'){
+        if(child.props.children === '' || typeof child.props.children === 'undefined' || child.props.children.length === 0){
           allTheChildren.push(React.createElement(child.type, keyProps, content));
         }
         else {
